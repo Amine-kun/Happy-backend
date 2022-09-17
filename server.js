04c04bb0d6939e889b49c345671866609af5af1a
Amine-kun <aminehlab@gmail.com>
2022-09-17 17:21:27 +0200

    adding the front end as trusted

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ const { Server } = require("socket.io");
 const server = http.createServer(app);
 const io = new Server(server, {
   cors:{
-    origin: "http://localhost:3000",
+    origin: "https://web-happy.herokuapp.com/",
     methods:["GET", "POST"],
   }
 });
