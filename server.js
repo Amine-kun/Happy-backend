f4c73bc3acb76be3e525b2346289c0db0c1a74ce
Amine-kun <aminehlab@gmail.com>
2022-09-20 14:41:59 +0200

    socket io config

--- server.js
+++ server.js
@@ -15,9 +15,9 @@ const { Server } = require("socket.io");
 const server = http.createServer(app);
 const io = new Server(server, {
   cors:{
-    origin: "*",
-    // methods:["GET", "POST"],
-    // credentials: true,
+    origin: "http://localhost:3000/",
+    methods:["GET", "POST"],
+    credentials: true,
   }
 });
 
